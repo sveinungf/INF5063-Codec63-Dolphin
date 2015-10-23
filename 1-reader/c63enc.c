8f1bfb718520ce01a7a9bd2fa4cf6fef24f69e29
Halvor Kielland-Gyrud <h.kiella@gmail.com>
2015-10-23 23:10:58 +0200

    Started adding Dolpin API calls

--- 1-reader/c63enc.c
+++ 1-reader/c63enc.c
@@ -14,278 +14,334 @@
 #include "me.h"
 #include "tables.h"
 
-static char *output_file, *input_file;
-FILE *outfile;
+static sci_desc_t vd;
+
+static unsigned int localAdapterNo;
+static unsigned int remoteNodeId;
+
+static unsigned int remoteSegmentId_Y;
+static unsigned int remoteSegmentId_U;
+static unsigned int remoteSegmentId_V;
+
+static sci_remote_segment_t remoteSegment_Y;
+static sci_remote_segment_t remoteSegment_U;
+static sci_remote_segment_t remoteSegment_V;
+
+static unsigned int segmentSize_Y;
+static unsigned int segmentSize_U;
+static unsigned int segmentSize_V;
+
+static void *localMapAddr_Y;
+static void *localMapAddr_U;
+static void *localMapAddr_V;
+
+static sci_map_t localMap_Y;
+static sci_map_t localMap_U;
+static sci_map_t localMap_V;
+
+static sci_map_t remoteMap_Y;
+static sci_map_t remoteMap_U;
+static sci_map_t remoteMap_V;
+
+sci_sequence_t sequence_Y;
+sci_sequence_t sequence_U;
+sci_sequence_t sequence_V;
+
+static char *input_file;
 
 static int limit_numframes = 0;
 
+yuv_t *image;
+yuv_t *image2;
+
+uint8_t *remote_Y;
+uint8_t *remote_U;
+uint8_t *remote_V;
+
 static uint32_t width;
 static uint32_t height;
+static int ypw, yph;
+static int upw, uph;
+static int vpw, vph;
 
 /* getopt */
 extern int optind;
 extern char *optarg;
 
 /* Read planar YUV frames with 4:2:0 chroma sub-sampling */
-static yuv_t* read_yuv(FILE *file, struct c63_common *cm)
-{
-  size_t len = 0;
-  yuv_t *image = malloc(sizeof(*image));
+static int read_yuv(FILE *file) {
+	size_t len = 0;
 
-  /* Read Y. The size of Y is the same as the size of the image. The indices
-     represents the color component (0 is Y, 1 is U, and 2 is V) */
-  image->Y = calloc(1, cm->padw[Y_COMPONENT]*cm->padh[Y_COMPONENT]);
-  len += fread(image->Y, 1, width*height, file);
+	/* Read Y. The size of Y is the same as the size of the image. The indices
+	 represents the color component (0 is Y, 1 is U, and 2 is V) */
+	len += fread(image->Y, 1, width * height, file);
 
-  /* Read U. Given 4:2:0 chroma sub-sampling, the size is 1/4 of Y
-     because (height/2)*(width/2) = (height*width)/4. */
-  image->U = calloc(1, cm->padw[U_COMPONENT]*cm->padh[U_COMPONENT]);
-  len += fread(image->U, 1, (width*height)/4, file);
+	/* Read U. Given 4:2:0 chroma sub-sampling, the size is 1/4 of Y
+	 because (height/2)*(width/2) = (height*width)/4. */
+	len += fread(image->U, 1, (width * height) / 4, file);
 
-  /* Read V. Given 4:2:0 chroma sub-sampling, the size is 1/4 of Y. */
-  image->V = calloc(1, cm->padw[V_COMPONENT]*cm->padh[V_COMPONENT]);
-  len += fread(image->V, 1, (width*height)/4, file);
+	/* Read V. Given 4:2:0 chroma sub-sampling, the size is 1/4 of Y. */
+	len += fread(image->V, 1, (width * height) / 4, file);
 
-  if (ferror(file))
-  {
-    perror("ferror");
-    exit(EXIT_FAILURE);
-  }
+	if (ferror(file)) {
+		perror("ferror");
+		exit(EXIT_FAILURE);
+	}
 
-  if (feof(file))
-  {
-    free(image->Y);
-    free(image->U);
-    free(image->V);
-    free(image);
+	if (feof(file)) {
+		return 0;
+	} else if (len != width * height * 1.5) {
+		fprintf(stderr, "Reached end of file, but incorrect bytes read.\n");
+		fprintf(stderr, "Wrong input? (height: %d width: %d)\n", height, width);
 
-    return NULL;
-  }
-  else if (len != width*height*1.5)
-  {
-    fprintf(stderr, "Reached end of file, but incorrect bytes read.\n");
-    fprintf(stderr, "Wrong input? (height: %d width: %d)\n", height, width);
+		return 0;
+	}
 
-    free(image->Y);
-    free(image->U);
-    free(image->V);
-    free(image);
-
-    return NULL;
-  }
-
-  return image;
+	return len;
 }
 
-static void c63_encode_image(struct c63_common *cm, yuv_t *image)
-{
-  /* Advance to next frame */
-  destroy_frame(cm->refframe);
-  cm->refframe = cm->curframe;
-  cm->curframe = create_frame(cm, image);
+static void init_c63_enc() {
+	ypw = (uint32_t) (ceil(width/16.0f)*16);
+	yph = (uint32_t) (ceil(height/16.0f)*16);
+	upw = (uint32_t) (ceil(width*UX /(YX*8.0f))*8);
+	uph = (uint32_t) (ceil(height*UY/(YY*8.0f))*8);
+	vpw = (uint32_t) (ceil(width*VX/(YX*8.0f))*8);
+	vph = (uint32_t) (ceil(height*VY/(YY*8.0f))*8);
 
-  /* Check if keyframe */
-  if (cm->framenum == 0 || cm->frames_since_keyframe == cm->keyframe_interval)
-  {
-    cm->curframe->keyframe = 1;
-    cm->frames_since_keyframe = 0;
-
-    fprintf(stderr, " (keyframe) ");
-  }
-  else { cm->curframe->keyframe = 0; }
-
-  if (!cm->curframe->keyframe)
-  {
-    /* Motion Estimation */
-    c63_motion_estimate(cm);
-
-    /* Motion Compensation */
-    c63_motion_compensate(cm);
-  }
-
-  /* DCT and Quantization */
-  dct_quantize(image->Y, cm->curframe->predicted->Y, cm->padw[Y_COMPONENT],
-      cm->padh[Y_COMPONENT], cm->curframe->residuals->Ydct,
-      cm->quanttbl[Y_COMPONENT]);
-
-  dct_quantize(image->U, cm->curframe->predicted->U, cm->padw[U_COMPONENT],
-      cm->padh[U_COMPONENT], cm->curframe->residuals->Udct,
-      cm->quanttbl[U_COMPONENT]);
-
-  dct_quantize(image->V, cm->curframe->predicted->V, cm->padw[V_COMPONENT],
-      cm->padh[V_COMPONENT], cm->curframe->residuals->Vdct,
-      cm->quanttbl[V_COMPONENT]);
-
-  /* Reconstruct frame for inter-prediction */
-  dequantize_idct(cm->curframe->residuals->Ydct, cm->curframe->predicted->Y,
-      cm->ypw, cm->yph, cm->curframe->recons->Y, cm->quanttbl[Y_COMPONENT]);
-  dequantize_idct(cm->curframe->residuals->Udct, cm->curframe->predicted->U,
-      cm->upw, cm->uph, cm->curframe->recons->U, cm->quanttbl[U_COMPONENT]);
-  dequantize_idct(cm->curframe->residuals->Vdct, cm->curframe->predicted->V,
-      cm->vpw, cm->vph, cm->curframe->recons->V, cm->quanttbl[V_COMPONENT]);
-
-  /* Function dump_image(), found in common.c, can be used here to check if the
-     prediction is correct */
-
-  write_frame(cm);
-
-  ++cm->framenum;
-  ++cm->frames_since_keyframe;
+	image = malloc(sizeof(*image));
+	image->Y = malloc(ypw*yph*sizeof(uint8_t));
+	image->U = malloc(upw*uph*sizeof(uint8_t));
+	image->V = malloc(vpw*vph*sizeof(uint8_t));
 }
 
-struct c63_common* init_c63_enc(int width, int height)
-{
-  int i;
+static sci_error_t init_SISCI() {
 
-  /* calloc() sets allocated memory to zero */
-  struct c63_common *cm = calloc(1, sizeof(struct c63_common));
+	// Initialization of SISCI API
+	sci_error_t error;
+	SCIInitialize(NO_FLAGS, &error);
+	if (error != SCI_ERR_OK) {
+		return error;
+	}
 
-  cm->width = width;
-  cm->height = height;
+	SCIOpen(&vd, NO_FLAGS, &error);
+	if (error != SCI_ERR_OK) {
+		return error;
+	}
 
-  cm->padw[Y_COMPONENT] = cm->ypw = (uint32_t)(ceil(width/16.0f)*16);
-  cm->padh[Y_COMPONENT] = cm->yph = (uint32_t)(ceil(height/16.0f)*16);
-  cm->padw[U_COMPONENT] = cm->upw = (uint32_t)(ceil(width*UX/(YX*8.0f))*8);
-  cm->padh[U_COMPONENT] = cm->uph = (uint32_t)(ceil(height*UY/(YY*8.0f))*8);
-  cm->padw[V_COMPONENT] = cm->vpw = (uint32_t)(ceil(width*VX/(YX*8.0f))*8);
-  cm->padh[V_COMPONENT] = cm->vph = (uint32_t)(ceil(height*VY/(YY*8.0f))*8);
 
-  cm->mb_cols = cm->ypw / 8;
-  cm->mb_rows = cm->yph / 8;
+	SCIGetLocalNodeId(localAdapterNo, &localNodeId, NO_FLAGS, &error);
+	if(error != SCI_ERROR_OK) {
+		return error;
+	}
 
-  /* Quality parameters -- Home exam deliveries should have original values,
-   i.e., quantization factor should be 25, search range should be 16, and the
-   keyframe interval should be 100. */
-  cm->qp = 25;                  // Constant quantization factor. Range: [1..50]
-  cm->me_search_range = 16;     // Pixels in every direction
-  cm->keyframe_interval = 100;  // Distance between keyframes
+	remoteSegmentId_Y = (remoteNodeId << 16) | localNodeId | Y_COMPONENT;
+	remoteSegmentId_U = (remoteNodeId << 16) | localNodeId | U_COMPONENT;
+	remoteSegmentId_V = (remoteNodeId << 16) | localNodeId | V_COMPONENT;
 
-  /* Initialize quantization tables */
-  for (i = 0; i < 64; ++i)
-  {
-    cm->quanttbl[Y_COMPONENT][i] = yquanttbl_def[i] / (cm->qp / 10.0);
-    cm->quanttbl[U_COMPONENT][i] = uvquanttbl_def[i] / (cm->qp / 10.0);
-    cm->quanttbl[V_COMPONENT][i] = uvquanttbl_def[i] / (cm->qp / 10.0);
-  }
+	do {
+		SCIConnectSegment(vd, &remoteSegment_Y, remoteNodeId, remoteSegmentId_Y, localAdapterNo,
+				NO_CALLBACK,NULL, SCI_INFINITE_TIMEOUT, NO_FLAGS, &error);
+	} while (error != SCI_ERR_OK);
 
-  return cm;
+	do {
+		SCIConnectSegment(vd, &remoteSegment_U, remoteNodeId, remoteSegmentId_U, localAdapterNo,
+				NO_CALLBACK,NULL, SCI_INFINITE_TIMEOUT, NO_FLAGS, &error);
+	} while (error != SCI_ERR_OK);
+
+	do {
+		 SCIConnectSegment(vd, &remoteSegment_V, remoteNodeId, remoteSegmentId_V, localAdapterNo,
+				 NO_CALLBACK,NULL, SCI_INFINITE_TIMEOUT, NO_FLAGS, &error);
+	} while (error != SCI_ERR_OK);
+
+	int offset = 0;
+
+	remote_Y = SCIMapRemoteSegment(remoteSegment_Y , &remoteMap_Y, offset, ypw*yph*sizeof(uint8_t), NULL,NO_FLAGS, &error);
+	if (error != SCI_ERROR_OK) {
+		return error;
+	}
+
+	remote_U = SCIMapRemoteSegment(remoteSegment_U , &remoteMap_U, offset, upw*uph*sizeof(uint8_t), NULL,NO_FLAGS, &error);
+	if (error != SCI_ERROR_OK) {
+			return error;
+	}
+
+	remote_V = SCIMapRemoteSegment(remoteSegment_V , &remoteMap_V, offset, vpw*vph*sizeof(uint8_t), NULL,NO_FLAGS, &error);
+	if (error != SCI_ERROR_OK) {
+			return error;
+	}
+
+	// Create sequences for data error checking
+	SCICreateMapSequence(remoteMap_Y, &sequence_Y, 0, &error);
+    if (error != SCI_ERR_OK) {
+    	fprintf(stderr,"SCICreateMapSequence failed - Error code 0x%x\n",error);
+    	return error;
+	}
+
+    SCICreateMapSequence(remoteMap_U, &sequence_U, 0, &error);
+    if (error != SCI_ERR_OK) {
+		fprintf(stderr,"SCICreateMapSequence failed - Error code 0x%x\n",error);
+		return error;
+	}
+
+    SCICreateMapSequence(remoteMap_V, &sequence_V, 0, &error);
+	if (error != SCI_ERR_OK) {
+		fprintf(stderr,"SCICreateMapSequence failed - Error code 0x%x\n",error);
+		return error;
+	}
+
+
+
+
+	return SCI_ERROR_OK;
 }
 
-void free_c63_enc(struct c63_common* cm)
-{
-  destroy_frame(cm->curframe);
-  free(cm);
+static sci_error_t cleanup_SISCI() {
+	SCIClose(&vd, NO_FLAGS, &error);
+	SCITerminate();
 }
 
-static void print_help()
-{
-  printf("Usage: ./c63enc [options] input_file\n");
-  printf("Commandline options:\n");
-  printf("  -h                             Height of images to compress\n");
-  printf("  -w                             Width of images to compress\n");
-  printf("  -o                             Output file (.c63)\n");
-  printf("  [-f]                           Limit number of frames to encode\n");
-  printf("\n");
+static void print_help() {
+	printf("Usage: ./c63enc [options] input_file\n");
+	printf("Commandline options:\n");
+	printf("  -h                             Height of images to compress\n");
+	printf("  -w                             Width of images to compress\n");
+	printf("  -a                             Local adapter number\n");
+	printf("  -r                             Remote node identifier\n");
+	printf(
+			"  [-f]                           Limit number of frames to encode\n");
+	printf("\n");
 
-  exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
-int main(int argc, char **argv)
-{
-  int c;
-  yuv_t *image;
+int main(int argc, char **argv) {
+	int c;
 
-  if (argc == 1) { print_help(); }
+	if (argc == 1) {
+		print_help();
+	}
 
-  while ((c = getopt(argc, argv, "h:w:o:f:i:")) != -1)
-  {
-    switch (c)
-    {
-      case 'h':
-        height = atoi(optarg);
-        break;
-      case 'w':
-        width = atoi(optarg);
-        break;
-      case 'o':
-        output_file = optarg;
-        break;
-      case 'f':
-        limit_numframes = atoi(optarg);
-        break;
-      default:
-        print_help();
-        break;
-    }
-  }
+	while ((c = getopt(argc, argv, "h:w:f:a:r:")) != -1) {
+		switch (c) {
+		case 'h':
+			height = atoi(optarg);
+			break;
+		case 'w':
+			width = atoi(optarg);
+			break;
+		case 'f':
+			limit_numframes = atoi(optarg);
+			break;
+		case 'a':
+			localAdapterNo = atoi(optarg);
+			break;
+		case 'r':
+			remoteNodeId = atoi(optarg);
+			break;
+		default:
+			print_help();
+			break;
+		}
+	}
 
-  if (optind >= argc)
-  {
-    fprintf(stderr, "Error getting program options, try --help.\n");
-    exit(EXIT_FAILURE);
-  }
+	if (optind >= argc) {
+		fprintf(stderr, "Error getting program options, try --help.\n");
+		exit(EXIT_FAILURE);
+	}
 
-  outfile = fopen(output_file, "wb");
+	init_c63_enc();
 
-  if (outfile == NULL)
-  {
-    perror("fopen");
-    exit(EXIT_FAILURE);
-  }
+	static sci_error_t error = init_SISCI();
+	if (error != SCI_ERROR_OK) {
+		fprintf(stderr, "Error initialising SISCI API - error code: %x\n", error);
+		exit(EXIT_FAILURE);
+	}
 
-  struct c63_common *cm = init_c63_enc(width, height);
-  cm->e_ctx.fp = outfile;
+	input_file = argv[optind];
 
-  input_file = argv[optind];
+	if (limit_numframes) {
+		printf("Limited to %d frames.\n", limit_numframes);
+	}
 
-  if (limit_numframes) { printf("Limited to %d frames.\n", limit_numframes); }
+	FILE *infile = fopen(input_file, "rb");
 
-  FILE *infile = fopen(input_file, "rb");
+	if (infile == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
 
-  if (infile == NULL)
-  {
-    perror("fopen");
-    exit(EXIT_FAILURE);
-  }
+	/* Encode input frames */
+	int numframes = 0;
 
-  /* Encode input frames */
-  int numframes = 0;
+	int rc;
+	while (1) {
+		rc = read_yuv(infile);
 
-  while (1)
-  {
-    image = read_yuv(infile, cm);
+		if (!rc) {
+			// TODO: Signal computation node that there are no more frames to be encoded
+			break;
+		}
 
-    if (!image) { break; }
+		if (numframes != 0) {
+			// Wait for interrupt
 
-    printf("Encoding frame %d, ", numframes);
-    c63_encode_image(cm, image);
+		}
 
-    free(image->Y);
-    free(image->U);
-    free(image->V);
-    free(image);
+		// Copy new frame to remote segment
+		printf("Sending frame %d to computation node, ", numframes);
 
-    printf("Done!\n");
+		int remoteOffset = 0;
 
-    ++numframes;
+		SCIMemCpy(sequence_Y, image->Y, remoteMap_Y, remoteOffset, ypw*yph*sizeof(uint8_t), SCI_FLAG_ERROR_CHECK, &error);
+		if(error != SCI_ERROR_OK) {
+			// Do something
+		}
 
-    if (limit_numframes && numframes >= limit_numframes) { break; }
-  }
+		SCIMemCpy(sequence_U, image->U, remoteMap_U, remoteOffset, upw*uph*sizeof(uint8_t), SCI_FLAG_ERROR_CHECK, &error);
+		if(error != SCI_ERROR_OK) {
+			// Do something
+		}
 
-  free_c63_enc(cm);
-  fclose(outfile);
-  fclose(infile);
+		SCIMemCpy(sequence_V, image->V, remMapMap_V, remoteOffset, vpw*vph*sizeof(uint8_t), SCI_FLAG_ERROR_CHECK, &error);
+		if(error != SCI_ERROR_OK) {
+			// Do something
+		}
 
-  //int i, j;
-  //for (i = 0; i < 2; ++i)
-  //{
-  //  printf("int freq[] = {");
-  //  for (j = 0; j < ARRAY_SIZE(frequencies[i]); ++j)
-  //  {
-  //    printf("%d, ", frequencies[i][j]);
-  //  }
-  //  printf("};\n");
-  //}
+		printf("Done!\n");
 
-  return EXIT_SUCCESS;
+		// Send interrupt to computation node signalling that the frame has been copied
+		// TODO: do
+
+		++numframes;
+
+		if (limit_numframes && numframes >= limit_numframes) {
+			// TODO: Signal computation node that there are no more frames to be encoded
+			break;
+		}
+	}
+
+	free(image->Y);
+	free(image->U);
+	free(image->V);
+	free(image);
+	fclose(infile);
+
+	error = cleanup_SISCI();
+	if (error != SCI_ERROR_OK) {
+		fprintf(stderr, "Error during SISCI cleanup - error code: %x\n", error);
+		exit(EXIT_FAILURE);
+	}
+
+	//int i, j;
+	//for (i = 0; i < 2; ++i)
+	//{
+	//  printf("int freq[] = {");
+	//  for (j = 0; j < ARRAY_SIZE(frequencies[i]); ++j)
+	//  {
+	//    printf("%d, ", frequencies[i][j]);
+	//  }
+	//  printf("};\n");
+	//}
+
+	return EXIT_SUCCESS;
 }

aeac79343a460807f92a8b5d43b4614671d40af4
ikoddn <ikonage@gmail.com>
2015-11-06 12:01:37 +0100

    Writer: Moved functions from io.c into c63_write.c and made them static inline

--- 3-writer/c63_write.c
+++ 3-writer/c63_write.c
@@ -9,10 +9,75 @@
 
 #include "c63.h"
 #include "c63_write.h"
-#include "io.h"
 #include "tables.h"
 
 int frequencies[2][12];
+
+static inline void put_byte(FILE *fp, int byte)
+{
+  int status = fputc(byte, fp);
+
+  if (status == EOF)
+  {
+    fprintf(stderr, "Error writing byte\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+static inline void put_bytes(FILE *fp, const void* data, unsigned int len)
+{
+  size_t n = fwrite(data, 1, (size_t) len, fp);
+
+  if(n != (size_t) len)
+  {
+    fprintf(stderr, "Error writing bytes\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+/**
+ * Adds a bit to the bitBuffer. A call to bit_flush() is needed
+ * in order to write any remainding bits in the buffer before
+ * writing using another function.
+ */
+static inline void put_bits(struct entropy_ctx *c, uint16_t bits, uint8_t n)
+{
+  assert(n <= 24  && "Error writing bit");
+
+  if(n == 0) { return; }
+
+  c->bit_buffer <<= n;
+  c->bit_buffer |= bits & ((1 << n) - 1);
+  c->bit_buffer_width += n;
+
+  while(c->bit_buffer_width >= 8)
+  {
+    uint8_t b = (uint8_t)(c->bit_buffer >> (c->bit_buffer_width - 8));
+
+    put_byte(c->fp, b);
+
+    if(b == 0xff) { put_byte(c->fp, 0); }
+
+    c->bit_buffer_width -= 8;
+  }
+}
+
+/**
+ * Flushes the bitBuffer by writing zeroes to fill a full byte
+ */
+static inline void flush_bits(struct entropy_ctx *c)
+{
+  if(c->bit_buffer > 0)
+  {
+    uint8_t b = c->bit_buffer << (8 - c->bit_buffer_width);
+    put_byte(c->fp, b);
+
+    if(b == 0xff) { put_byte(c->fp, 0); }
+  }
+
+  c->bit_buffer = 0;
+  c->bit_buffer_width = 0;
+}
 
 /* Start of Image (SOI) marker, contains no payload. */
 static void write_SOI(struct c63_common *cm)

c271d40c50e55370f8b16a767a99343ea9a6d62a
Halvor Kielland-Gyrud <h.kiella@gmail.com>
2015-11-18 02:21:34 +0100

    asd

--- 3-writer/write.h
+++ 3-writer/write.h
@@ -5,7 +5,7 @@
 
 #include "c63.h"
 
-void write_frame_to_buffer(struct c63_common *cm, std::vector<uint8_t>& byte_vector);
+void write_frame_to_buffer(struct c63_common *cm, std::vector<uint8_t>& byte_vector, int threadNum);
 
 void write_buffer_to_file(const std::vector<uint8_t>& byte_vector, FILE* file);
 
